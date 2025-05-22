b9ddc3e4c83adb0cdd17bd937e99aae675f57f96
Mikael-gapit <98390707+Mikael-gapit@users.noreply.github.com>
2025-05-23 00:39:08 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,6 @@
-
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open('stoy-cache').then(cache => {
-    return cache.addAll([
-      './',
-      './index.html',
-      './manifest.json',
-      './icon-192.png',
-      './icon-512.png'
-    ]);
-  }));
+  event.waitUntil(caches.open('cache').then(cache => cache.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));
 });
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
